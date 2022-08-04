9cd0eca923016595d7944716a0638a463c4e822a
Pedro-Dom <pedroaugusto.dom@outlook.com>
2022-08-03 22:13:13 -0300

    Progresso 03/08

---
+++ 7.Objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Príncipios importantes:
+// 1. Abstração - Trazer o objeto da vida real para o software
+// 2. Encapsulamento - Esconder detalhes de implementação e expor uma interface de comunicação simplificada
+// 3. Herança (prototype) - Receber atributos e comportamentos(funções) de um objeto pai
+// 4. Polimorfismo - A partir de uma variável mais genérica, atribuir à uma variável mais específica
